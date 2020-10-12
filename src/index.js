34e5c3606770282308da0ff67bbee478bf657247
alexayalauxd <alexayala.esp@gmail.com>
2020-10-12 16:56:10 -0500

    new

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Init App');
+import Amplify, { API } from "@aws-amplify/api";
+
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
+
+window.onload = function() {
+    API.get('payAPI', '/items', {}).then(x=>{
+        console.log(x);    
+    })
+}
